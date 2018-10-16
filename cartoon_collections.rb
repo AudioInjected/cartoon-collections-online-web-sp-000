4de0b31d17a72e6441fe34cc9fc091b09a824772
Rafael Batista <rafaelb24@yahoo.com>
2018-10-16 21:16:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(food)
    return food.include?(cheese_types)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 end
